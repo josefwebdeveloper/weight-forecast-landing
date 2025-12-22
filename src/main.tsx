07b2263a7143039936536522f0475d3182acec0f
Josef Shainskiy <josef2011@gmail.com>
2025-12-22 09:37:32 +0200

    Add Privacy Policy and Terms of Service pages with contact email

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import Privacy from './Privacy';
+import Terms from './Terms';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
